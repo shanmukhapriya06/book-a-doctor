b6afa4d2875d3d82489ade2df17410345fdf3926
Shanmukhapriya <shanmukhapriya2006@gmail.com>
2026-06-23 18:23:32 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8001/api',
+  baseURL: 'https://YOUR-RENDER-URL.onrender.com/api',
   maxBodyLength: 10 * 1024 * 1024,
   maxContentLength: 10 * 1024 * 1024,
 });
